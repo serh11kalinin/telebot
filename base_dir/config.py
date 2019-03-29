32cc352efa282a1d6ded19cd49fbf60c1cc4f40d
PlayaBaller <43369009+PlayaBaller@users.noreply.github.com>
2019-03-29 17:52:08 +0200

    still WIP

--- base_dir/config.py
+++ base_dir/config.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
+import sqlite3
 
 token = '761517412:AAEWk1OLF29TjdWGyuy4WUmMru_em3HIhns'
 database_name = 'music.db'  # DB file
+# database_name = sqlite3.connect('music.db')
 shelve_name = 'shelve.db'  # storage file
