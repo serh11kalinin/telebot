e6cae48aad51995025daa4bc3f32b54842c843e3
PlayaBaller <43369009+PlayaBaller@users.noreply.github.com>
2019-04-01 20:41:36 +0300

    TODO: re-implement DB file

--- base_dir/bot.py
+++ base_dir/bot.py
@@ -16,6 +16,7 @@ def repeat_all_messages(message):
     bot.send_message(message.chat.id, message.text)
 
 
+
 if __name__ == '__main__':
     bot.polling(none_stop=True)
 """
@@ -63,6 +64,7 @@ def find_file_ids(message):
         if file.split('.')[-1] == 'ogg':
             f = open('music/' + file, 'rb')
             msg = bot.send_voice(message.chat.id, f, None)
+            print(msg)
             # sending file_id after file itself
             bot.send_message(message.chat.id, msg.voice.file_id, reply_to_message_id=msg.message_id)
         time.sleep(3)
